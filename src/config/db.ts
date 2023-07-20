65a7642457ec31755882c0543668f8e7cb82df50
sammyalwayscode <olorundasamuel2@gmail.com>
2023-07-20 16:51:58 +0100

    updated(Database)

---
+++ src/config/db.ts
@@ -0,0 +1,30 @@
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = "mongodb://127.0.0.1:27017/profileApp";
+
+// mongoose.connect(MONGODB_URI);
+// mongoose.connection
+//   .on("open", () => {
+//     console.log("Database Connected");
+//   })
+//   .once("error", () => {
+//     console.log("Failed to connect to database");
+//   });
+
+import mongoose from "mongoose";
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/myapp";
+
+const dbConfig = () => {
+  mongoose.connect(MONGODB_URI);
+
+  mongoose.connection
+    .on("open", () => {
+      console.log("Database Connected 🔥🔥🔥");
+    })
+    .once("error", () => {
+      console.log("Error Connecting to database");
+    });
+};
+
+export default dbConfig;
